6b84aa75750aa4ae5b2d7f59680e136056e53d2a
Deepthi <Deepthi@DESKTOP-RMSCL2B>
2022-03-16 17:54:50 +0530

    @5.54pm

---
+++ src/test/java/mavenparametertest/orgTest.java
@@ -0,0 +1,47 @@
+package mavenparametertest;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+	
+@Test
+	
+	public void orgtest_1() {
+		
+		System.out.println("org_1 is created");
+		
+	}
+	
+	@Test
+	
+public void orgttest_2() {
+		
+		System.out.println("org_2 is created");
+		
+	}
+	
+	@Test
+
+public void orgttest_3() {
+		
+		System.out.println("org_3 is created");
+		
+	}
+	
+	@Test
+	
+public void orgttest_4() {
+		
+		System.out.println("org_4SSSss is created");
+		
+		System.out.println("org_4SSSss is created");
+		
+		System.out.println("org_6ssssss is created");
+		
+		
+		
+	}
+
+
+}
